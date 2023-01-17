b27f2eeb9cb945753deee24dc63f9eaf28b3a0c1
Mike Cifelli <1836280-mike-cifelli@users.noreply.gitlab.com>
2023-01-17 10:39:35 -0500

    Remove unset voltage from reading

--- main.py
+++ main.py
@@ -76,6 +76,7 @@ try:
   #   del reading["temperature"]        # remove the temperature reading
   #
   #   reading["custom"] = my_reading()  # add my custom reading value
+  del reading["voltage"]
 
   # is an upload destination set?
   if enviro.config.destination:
